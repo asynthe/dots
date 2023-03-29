638627e72616079758216768d5cd06ad41b7bdb1
Benjamin Dunstan <benjadusntan@gmail.com>
2023-03-29 15:33:43 +0800

    third commit

--- chemacs/profiles.el
+++
@@ -1,9 +0,0 @@
-;; default
-(("emacs" . ((user-emacs-directory . "~/.emacs.default")
-               (server-name . "emacs")
-               ))
-
-;; doom emacs
-  ("doom" . ((user-emacs-directory . "~/.doom-emacs")
-             (server-name . "doom")
-           (env . (("DOOMDIR" . "~/sync/system/config/emacs/doom-config"))))))
